eb9c8872ee91897e6edd6d9d6052a20699115b49
WorldFS <fsworld009@gmail.com>
2013-07-22 19:00:52 -0500

    add chat input and send button

--- src/jtchat/gui/Chatroom.java
+++ src/jtchat/gui/Chatroom.java
@@ -1,7 +1,12 @@
 
 package jtchat.gui;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 
 public class Chatroom extends JFrame{
@@ -15,6 +20,17 @@ public class Chatroom extends JFrame{
     
     //initialize UI components
     private void init(){
+        //BorderLayout layoutMgr = 
         
+        JPanel textBoxPanel = new JPanel();
+        textBoxPanel.setLayout(new FlowLayout());
+        
+        JTextField inputField = new JTextField(14);
+        JButton sendButton = new JButton("Chat");
+        textBoxPanel.add(inputField);
+        textBoxPanel.add(sendButton);
+        
+        this.setLayout(new BorderLayout());
+        this.add(textBoxPanel,BorderLayout.SOUTH);
     }
 }
