6e93b2c1e9d2f4fc86f63ecaedd701f7666051d3
WorldFS <fsworld009@gmail.com>
2013-08-25 20:00:58 -0500

    find a fix for not wrapping long words on JTextPane under jre7, need to refine gui classes, save log ,and save/load profilesbefore release

--- src/jtchat/gui/MainWindow.java
+++ src/jtchat/gui/MainWindow.java
@@ -2,13 +2,10 @@
 package jtchat.gui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -21,9 +18,18 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.SwingUtilities;
-import javax.swing.text.BadLocationException;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.BoxView;
+import javax.swing.text.ComponentView;
+import javax.swing.text.Element;
+import javax.swing.text.IconView;
+import javax.swing.text.LabelView;
+import javax.swing.text.ParagraphView;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledEditorKit;
+import javax.swing.text.View;
+import javax.swing.text.ViewFactory;
 import jtchat.irc.ChatMsgListener;
 import jtchat.irc.JtvIRCBot;
 
@@ -180,11 +186,14 @@ public class MainWindow extends JFrame implements ChatMsgListener{
         //textBoxP2.setOpaque(false);
         textBoxPanel.setOpaque(false);
         chatMsgsPane.setOpaque(false);
+        chatMsgsPane.setEditorKit(new WrapEditorKit());
 
         chatScrollPane.getViewport().setOpaque(false);
         chatScrollPane.setOpaque(false);
 
+        chatMsgsPane.setBorder(null);
         chatScrollPane.setBorder(null);
+        
         
         this.add(chatScrollPane,BorderLayout.CENTER);
         
@@ -347,6 +356,55 @@ public class MainWindow extends JFrame implements ChatMsgListener{
             
         }
         
+    }
+    
+    //http://java-sl.com/tip_letter_wrap_java7.html
+    class WrapEditorKit extends StyledEditorKit {
+        ViewFactory defaultFactory=new WrapColumnFactory();
+        public ViewFactory getViewFactory() {
+            return defaultFactory;
+        }
+ 
+    }
+ 
+    class WrapColumnFactory implements ViewFactory {
+        public View create(Element elem) {
+            String kind = elem.getName();
+            if (kind != null) {
+                if (kind.equals(AbstractDocument.ContentElementName)) {
+                    return new WrapLabelView(elem);
+                } else if (kind.equals(AbstractDocument.ParagraphElementName)) {
+                    return new ParagraphView(elem);
+                } else if (kind.equals(AbstractDocument.SectionElementName)) {
+                    return new BoxView(elem, View.Y_AXIS);
+                } else if (kind.equals(StyleConstants.ComponentElementName)) {
+                    return new ComponentView(elem);
+                } else if (kind.equals(StyleConstants.IconElementName)) {
+                    return new IconView(elem);
+                }
+            }
+ 
+            // default to text display
+            return new LabelView(elem);
+        }
+    }
+ 
+    class WrapLabelView extends LabelView {
+        public WrapLabelView(Element elem) {
+            super(elem);
+        }
+ 
+        public float getMinimumSpan(int axis) {
+            switch (axis) {
+                case View.X_AXIS:
+                    return 0;
+                case View.Y_AXIS:
+                    return super.getMinimumSpan(axis);
+                default:
+                    throw new IllegalArgumentException("Invalid axis: " + axis);
+            }
+        }
+ 
     }
     
     
